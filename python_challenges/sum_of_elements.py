41f24e577fa6c716bcb7e5d734da925eda899909
mags337 <113241049+mags337@users.noreply.github.com>
2024-01-15 14:56:26 +0100

    script to count the sum of numerical elements in a list

---
+++ python_challenges/sum_of_elements.py
@@ -0,0 +1,19 @@
+'Write a program to find the sum of all elements in a list.'
+
+def sum_of_elements(input_list):
+    try:
+        'Filtering the non-numerical elements in the given list'
+        input = [element for element in input_list if isinstance(element, (int,float))]
+        return sum(input)
+    except ValueError:
+        return "ValueError"
+
+if __name__ == '__main__':      
+    try: 
+        'Get the user input and convert it to a list of floats, then calculate and save the sum of the list elements'
+        user_input = [float(item) for item in list(input("Please enter your list of elements here (comma-seperated): ").split(","))]
+        result = sum_of_elements(user_input)
+        print(f' The sum of the list {user_input} is: {result}')
+    
+    except ValueError:
+        print("----ValueError. Input elements must be int or float types!----")
