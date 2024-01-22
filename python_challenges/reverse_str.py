b2077b9d26fd9f6b055c0516ba82ba97b12af115
mags337 <113241049+mags337@users.noreply.github.com>
2024-01-22 10:28:57 +0100

    program to reverse the order of a string

--- python_challenges/reverse_str.py
+++ python_challenges/reverse_str.py
@@ -1,10 +1,24 @@
 'Write a program to reverse a given string.'
 
 def reverse_string(string):
+    '''
+    Reverses the order of characters in a given string.
+
+    Parameters:
+    - string (str): The input string to be reversed.
+    Returns:
+    - str: The reversed string.
+    Raises:
+    - ValueError: If the input is not a string.
+    '''
     try:
+        # Use list comprehension to create a reversed version of the string
+        # by iterating over its characters in reverse order.
         rev = [string[-i-1] for i,letter in enumerate(string)]
+        # Join the reversed characters into a new string.
         return ''.join(map(str, rev))
     except ValueError:
+        # Handle the case where the input is not a string.
         return "ValueError: must be a string"
     
 
